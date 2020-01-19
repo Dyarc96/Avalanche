39f2b1afaf3192bf991114d2e08223022eff5e11
JDyja96 <jan.dyja@futurum.tech>
2020-01-19 15:56:57 +0100

    working example

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,37 @@
 const Controller = function() {
+    this.left = new Controller.ButtonInput();
+    this.right = new Controller.ButtonInput();
+    this.up = new Controller.ButtonInput();
 
-    this.onkeydown = function(keycode) {
+    this.onkeydown = function(keytype, keycode) {
+        let down = keytype === 'keydown' ? true : false;
         switch(keycode) {
             case 37:
-                console.log('arrow right');
+                this.left.getInput(down);
                 break;
             case 38:
-                console.log('arrow up');
+                this.up.getInput(down);
                 break;
             case 39:
-                console.log('arrow left');
-                break;
+                this.right.getInput(down);
         }
-    }
+    };
+};
+
+Controller.prototype = {
+    constructor: Controller
 }
+
+Controller.ButtonInput = function() {
+    this.active = this.down = false;
+}
+
+Controller.ButtonInput.prototype = {
+    constructor: Controller.ButtonInput,
+
+    getInput: function(down) {
+        if(this.down !== down)
+            this.active = down;
+        this.down = down;
+    }
+}; 
--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
 window.onload = () => {
-
-    let display = new Display(document.querySelector('canvas'));
-    let controller = new Controller();
-    let engine = new Engine();
-
-    function onKeyDown (keycode){
-        controller.onkeydown(keycode)
+    "use strict"
+    
+    var onKeyDown = function(keytype, keycode){
+        controller.onkeydown(keytype, keycode);
     }
 
-    window.addEventListener('resize', () => {
-        display.resize();
+    var resize = function(event) {
+        display.resize(document.documentElement.clientWidth - 32, document.documentElement.clientHeight - 32, game.world.height / game.world.width);
         display.render();
-    });
-
-    let render = function() {
-        display.fill(game)
     }
+
+    var render = function() {
+        display.fill(game.world.background_color);
+        display.drawRectangle(game.world.player.x, game.world.player.y, game.world.player.width, game.world.player.height, game.world.player.color);
+        display.render();
+    }
+
+    var update = function() {
+        if(controller.left.active) {
+            game.world.player.moveLeft();
+        }
+        if(controller.right.active) {
+            game.world.player.moveRight();
+        }
+        if(controller.up.active) {
+            game.world.player.jump();
+        }
+
+        game.update();
+    }
+
+    var display = new Display(document.querySelector('canvas'));
+    var controller = new Controller();
+    var engine = new Engine(1000/30, render, update);
+    var game = new Game();
+
+    display.buffer.canvas.height = game.world.height;
+    display.buffer.canvas.width = game.world.width;
 
     window.addEventListener('keydown', e => {
-        onKeyDown(e.keyCode);
+        onKeyDown(e.type, e.keyCode);
     });
+
+    window.addEventListener('keyup', e => {
+        onKeyDown(e.type, e.keyCode);
+    });
+
+    window.addEventListener('resize', () => {
+        resize();
+    });
+
+    resize();
 
     engine.start();
 }
